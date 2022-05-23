86aea7436f7123b95ff95734c2f22f826df42bd9
Lennart Ochel <lennart.ochel@ri.se>
2022-05-23 10:08:33 +0200

    Delete tests for now

--- src/dss/server/hexacopter_test.py
+++
@@ -1,9 +0,0 @@
-from dss.server.hexacopter import Waypoint
-
-
-def test_get_3D_distance():
-  stockholm = Waypoint(59.334591, 18.063240, 19)
-  norrkoping = Waypoint(58.588455, 16.188313, 12)
-  (_, _, d_alt, _, _, bearing) = norrkoping.get_3D_distance_to(stockholm)
-  assert d_alt == 7
-  assert 52 <= bearing <= 53
